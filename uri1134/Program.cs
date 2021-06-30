f503fb21cc7df0af79da3b4af1179ab6f1ba858d
Andrei Souza <andrey>
2021-06-30 11:42:40 -0300

    resolvendo problemas do uri online

---
+++ uri1134/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace uri1134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+            
+            int codigo = int.Parse(Console.ReadLine());
+
+            while(codigo != 4)
+            {
+                if(codigo == 1)
+                {
+                    alcool++;
+                }
+                else if(codigo == 2)
+                {
+                    gasolina++;
+                }
+                else if(codigo == 3)
+                {
+                    diesel++;
+                }
+                codigo = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Muito obrigado");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+        }
+    }
+}
